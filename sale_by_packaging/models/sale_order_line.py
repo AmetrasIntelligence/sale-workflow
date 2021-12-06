bfeaf3e3852ed3eae2df2ee8b1b6f27c46596614
Bastian Guenther <Bastian.Guenther@ametras.com>
2021-12-06 18:29:05 +0100

    [IMP] sale_by_packaging: * Added rounding functionality * Now also forcing quantity when option "force sale quantity" is enabled on package but option "only sell by packaging" is not enabled on the product * Now prefilling packaging when optin "only sell by packaging" is enabled on the product * Optimized view

--- sale_by_packaging/models/sale_order_line.py
+++ sale_by_packaging/models/sale_order_line.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 from odoo import _, api, models
 from odoo.exceptions import ValidationError
-from odoo.tools import float_compare
+from odoo.tools import float_compare, float_round
 
 
 class SaleOrderLine(models.Model):
@@ -74,8 +74,9 @@ class SaleOrderLine(models.Model):
         self.product_uom_qty = qty
         return True
 
-    @api.onchange("product_uom_qty")
+    @api.onchange("product_uom_qty", "product_id")
     def _onchange_product_uom_qty(self):
+        self._force_packaging()
         self._force_qty_with_package()
         res = super()._onchange_product_uom_qty()
         return res
@@ -100,25 +101,36 @@ class SaleOrderLine(models.Model):
 
     def write(self, vals):
         """Auto assign packaging if needed"""
-        if "product_packaging" in vals.keys() or self.env.context.get(
-            "_skip_auto_assign"
-        ):
+        if vals.get("product_packaging") or self.env.context.get("_skip_auto_assign"):
             # setting the packaging directly, skip auto assign
             return super().write(vals)
         for line in self:
             line_vals = vals.copy()
-            line_vals.update(line._write_auto_assign_packaging(line_vals))
+            packaging = self._get_autoassigned_packaging(line_vals)
+            if packaging:
+                line_vals.update({"product_packaging": packaging})
             super(SaleOrderLine, line).write(line_vals)
         return True
 
-    def _write_auto_assign_packaging(self, vals):
-        self.ensure_one()
+    @api.model
+    def create(self, vals):
+        """Auto assign packaging if needed"""
+        # Fill the packaging if they are empty and the quantity is a multiple
+        if not vals.get("product_packaging"):
+            packaging = self._get_autoassigned_packaging(vals)
+            if packaging:
+                vals.update({"product_packaging": packaging})
+        return super().create(vals)
+
+    def _get_autoassigned_packaging(self, vals=None):
+        if not vals:
+            vals = []
         product = (
             self.env["product.product"].browse(vals["product_id"])
             if "product_id" in vals
             else self.product_id
         )
-        if product:
+        if product and product.sell_only_by_packaging:
             quantity = (
                 vals["product_uom_qty"]
                 if "product_uom_qty" in vals
@@ -129,44 +141,50 @@ class SaleOrderLine(models.Model):
                 if "product_uom" in vals
                 else self.product_uom
             )
-            # Here, we ensure that no package is already set on the line.
-            # If so, it could lead to errors, since product_packaging_qty
-            # isn't updated after product_packaging has been modified.
-            # The simple way to handle that is to not modify product_packaging
-            # if one is already set.
-            if not self.product_packaging:
-                packaging = self._get_product_packaging_having_multiple_qty(
-                    product, quantity, uom
-                )
-                if packaging:
-                    return {"product_packaging": packaging.id}
-            # No need to raise an error here if no packaging has been found
-            #  since the error on _check_product_packaging will be raised
-        return {}
-
-    @api.model
-    def create(self, vals):
-        """Auto assign packaging if needed"""
-        # Fill the packaging if they are empty and the quantity is a multiple
-        if not vals.get("product_packaging"):
-            vals.update(self._create_auto_assign_packaging(vals))
-        return super().create(vals)
-
-    @api.model
-    def _create_auto_assign_packaging(self, vals):
-        product = (
-            self.env["product.product"].browse(vals["product_id"])
-            if "product_id" in vals
-            else False
-        )
-        if product and product.sell_only_by_packaging:
-            quantity = vals.get("product_uom_qty")
-            uom = self.env["uom.uom"].browse(vals.get("product_uom"))
             packaging = self._get_product_packaging_having_multiple_qty(
                 product, quantity, uom
             )
             if packaging:
-                return {"product_packaging": packaging.id}
-            # No need to raise an error here if no packaging has been found
-            #  since the error on _check_product_packaging will be raised
-        return {}
+                return packaging.id
+        return None
+
+    def _force_packaging(self):
+        if not self.product_packaging and self.product_id.sell_only_by_packaging:
+            packaging_id = self._get_autoassigned_packaging()
+            if packaging_id:
+                self.product_packaging = packaging_id
+
+    def _check_package(self):
+        if self.product_packaging:
+            rounded_up_qty = float_round(
+                self.product_packaging_qty,
+                precision_rounding=self.product_packaging.sale_rounding,
+                rounding_method="UP",
+            )
+            if self.product_packaging_qty != rounded_up_qty:
+                self.product_packaging_qty = rounded_up_qty
+                q = self.product_id.uom_id._compute_quantity(
+                    self.product_packaging.actual_sale_qty, self.product_uom
+                )
+                qty = self.product_qty
+                if qty and q and round(qty % q, 2):
+                    new_qty = qty - (qty % q) + q
+                    return {
+                        "warning": {
+                            "title": _("Warning"),
+                            "message": _(
+                                "Increased quantity to %.2f %s since package '%s' "
+                                "contains %.2f %s and can only be splitted "
+                                "into %s pieces."
+                            )
+                            % (
+                                new_qty,
+                                self.product_id.uom_id.name,
+                                self.product_packaging.name,
+                                self.product_packaging.qty,
+                                self.product_id.uom_id.name,
+                                round(1 / self.product_packaging.sale_rounding, 0),
+                            ),
+                        },
+                    }
+            return {}
