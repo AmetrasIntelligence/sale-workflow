6a98553918dc30f179f2599b2d76067cd4f0c16b
Bastian Guenther <Bastian.Guenther@ametras.com>
2022-01-21 16:15:47 +0100

    [IMP] sale_order_line_deep_sort: * Fixed, that sorting is not working on many2one fields * Added second sorting field

--- sale_order_line_deep_sort/__manifest__.py
+++ sale_order_line_deep_sort/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Order Line Deep Sort",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Sale Management",
