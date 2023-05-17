fc4c92341662a755eb6f12edaa1aa35a846558b1
rajasree.r@beo.in <rajasree.r@beo.in>
2023-05-17 14:33:34 +0530

    [FIX] sale_by_packaging: Removed packaging qty checking from constrain of sale order line.

--- sale_by_packaging/__manifest__.py
+++ sale_by_packaging/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Sale By Packaging",
     "summary": "Manage sale of packaging",
-    "version": "13.0.1.9.8",
+    "version": "13.0.1.9.9",
     "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/sale-workflow",
